bbb5d0a4e0b9805af9a8dc2f71557f36713c1ccb
vkray <vitalikkrainyuk@yandex.ru>
2023-06-21 12:45:35 +0300

    master. Вводное в Maven и Spring Boot + web

---
+++ src/test/java/pro/sky/blackwolncalculatordemo/BlackwolnCalculatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.blackwolncalculatordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackwolnCalculatorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
